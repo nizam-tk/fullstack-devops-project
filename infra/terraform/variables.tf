dcdb434444ab1fc4261727d7fd089dd74be4ecb3
Nizam <nizam.tk@outlook.com>
2025-11-11 08:34:23 +0530

    Add AZURE_* variable declarations to prevent Terraform Cloud warnings

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -35,3 +35,29 @@ variable "acr_name" {
   description = "Name of the Azure Container Registry"
   type        = string
 }
+
+# These variables are declared to avoid warnings from Terraform Cloud
+# but are not used since we use ARM_* environment variables instead
+variable "AZURE_CLIENT_ID" {
+  description = "Azure Service Principal Client ID (unused - using ARM_CLIENT_ID env var)"
+  type        = string
+  default     = ""
+}
+
+variable "AZURE_CLIENT_SECRET" {
+  description = "Azure Service Principal Client Secret (unused - using ARM_CLIENT_SECRET env var)"
+  type        = string
+  default     = ""
+}
+
+variable "AZURE_TENANT_ID" {
+  description = "Azure Tenant ID (unused - using ARM_TENANT_ID env var)"
+  type        = string
+  default     = ""
+}
+
+variable "AZURE_SUBSCRIPTION_ID" {
+  description = "Azure Subscription ID (unused - using ARM_SUBSCRIPTION_ID env var)"
+  type        = string
+  default     = ""
+}
