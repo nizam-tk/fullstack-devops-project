216bb19fe720d7f1f3b3fcbe2fbb258e5600d9ca
Nizam <nizam.tk@outlook.com>
2025-11-11 08:31:19 +0530

    Remove role assignment to fix authorization error and simplify authentication using environment variables

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -54,10 +54,5 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
-# Assign AcrPull role to AKS
-resource "azurerm_role_assignment" "aks_acr" {
-  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-  role_definition_name             = "AcrPull"
-  scope                            = azurerm_container_registry.acr.id
-  skip_service_principal_aad_check = true
-}
+# Note: AKS to ACR role assignment can be configured manually after deployment
+# or by granting the service principal 'User Access Administrator' role
--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -22,9 +22,6 @@ provider "azurerm" {
     }
   }
 
-  # Use service principal authentication
-  client_id       = var.azure_client_id
-  client_secret   = var.azure_client_secret
-  tenant_id       = var.azure_tenant_id
-  subscription_id = var.azure_subscription_id
+  # Authentication will use environment variables:
+  # ARM_CLIENT_ID, ARM_CLIENT_SECRET, ARM_TENANT_ID, ARM_SUBSCRIPTION_ID
 }
