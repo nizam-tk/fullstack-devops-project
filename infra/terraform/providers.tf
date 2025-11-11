8286d7ef44459d426e5897ed5df8a53268e226a6
Nizam <nizam.tk@outlook.com>
2025-11-11 09:16:41 +0530

    Force service principal auth only - disable CLI/MSI/OIDC authentication

--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -22,6 +22,11 @@ provider "azurerm" {
     }
   }
 
+  # Force service principal authentication and disable all other methods
+  use_cli                = false
+  use_msi                = false
+  use_oidc               = false
+
   # Use service principal authentication with environment variables
   # ARM_CLIENT_ID, ARM_CLIENT_SECRET, ARM_TENANT_ID, ARM_SUBSCRIPTION_ID
   # These are set in the Azure DevOps pipeline from variable groups
